f3cebdf11f4221557cfbf76cdc1a3370238842d9
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-30 18:26:01 +0200

    Modified

--- src/main/java/se/lexicon/model/VehicleType.java
+++ src/main/java/se/lexicon/model/VehicleType.java
@@ -2,73 +2,28 @@ package se.lexicon.model;
 
 public enum VehicleType {
 
-    private int id;
-    private String name;
-    private String phoneNumber;
-    private Reservation reservation;
+    CAR(1, "Car"),
+    MOTORCYCLE(2, "Motorcycle"),
+    TRUCK(3, "Truck"),
+    VAN(4, "Van"),
+    ELECTRIC(5, "EL"),
+    OTHER(6, "Other");
 
-    public Customer(String name, String phoneNumber) {
+    private final int code;
+    private final String name;
+
+    VehicleType(int code, String name) {
+        this.code = code;
         this.name = name;
-        this.phoneNumber = phoneNumber;
     }
 
-    public Customer(int id, String name, String phoneNumber) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Customer(int id, String name, String phoneNumber, Reservation reservation) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.reservation = reservation;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public int getCode() {
+        return code;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Reservation getReservation() {
-        return reservation;
-    }
-
-    public void setReservation(Reservation reservation) {
-        this.reservation = reservation;
-    }
-
-    @Override
-    public String toString() {
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("Customer Id: ").append(id)
-                .append(", Name: ").append(name)
-                .append(", PhoneNumber: ").append(phoneNumber);
-        if (reservation != null) {
-            sb.append(", Reservation Id: ").append(reservation.getId());
-        }
-
-        return sb.toString();
-
-    }
 }
